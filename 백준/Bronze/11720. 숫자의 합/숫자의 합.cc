5d36a20d09142ad9aae662ce4de04c9bd27dce79
nyungnim <nyu0kim@gmail.com>
2024-10-26 06:47:44 +0900

    [Bronze IV] Title: 숫자의 합, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N = 0;
+    string nums;
+    cin >> N;
+    cin >> nums;
+    
+    int sum = 0;
+    for (int i=0; i<nums.length(); i++) {
+        sum += nums[i] - '0';
+    }
+    cout << sum << '\n';
+}
