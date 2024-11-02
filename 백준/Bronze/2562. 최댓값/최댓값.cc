84ab7b9903109812f9f51741b866fd72118b4812
nyungnim <nyu0kim@gmail.com>
2024-11-02 15:08:15 +0900

    [Bronze III] Title: 최댓값, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.cc
+++ 백준/Bronze/2562. 최댓값/최댓값.cc
@@ -2,17 +2,18 @@
 using namespace std;
 
 int main()
-{    
+{
     int max_num = -1;
     int max_idx;
-    int num[9];
+    int num;
+    
     for(int i=0; i<9; i++) {
-        cin >> num[i];
-        if (max_num < num[i]) {
-            max_num = num[i];
+        cin >> num;
+        if(max_num < num) {
+            max_num = num;
             max_idx = i;
         }
     }
     cout << max_num << '\n';
-    cout << max_idx+1 << '\n';
+    cout << max_idx + 1<< '\n';
 }
