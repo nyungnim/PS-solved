66dd8f4aa4f607eb64ac072e486d27d672c387fa
nyungnim <nyu0kim@gmail.com>
2024-11-13 16:41:46 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.01 ms, Memory: 4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num) {
+    long long n = num;
+    int cnt = 0;
+    while(n != 1) {
+        if (cnt >= 500)
+            return -1;
+        if (n % 2 == 0) {
+            n /= 2;
+        } else {
+            n = n * 3 + 1;
+        }
+        cnt++;
+    }
+    
+    return cnt;
+}
